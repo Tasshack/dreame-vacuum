e35a4ed904fc7c2e8f7d0feda56c89c00864d591
Yiğit Topcu <yigit.topcu@cosa.com.tr>
2022-11-27 11:21:48 +0300

    Preliminary support for VSLAM robots

--- custom_components/dreame_vacuum/config_flow.py
+++ custom_components/dreame_vacuum/config_flow.py
@@ -77,15 +77,15 @@ SUPPORTED_MODELS = [
     "dreame.vacuum.p2036",
 
     # Dreame Vslam Robots
-    "dreame.vacuum.p2140q",
-    "dreame.vacuum.p2140p",
-    "dreame.vacuum.p2140o",
-    "dreame.vacuum.p2140a",
-    "dreame.vacuum.p2140",
-    "dreame.vacuum.p2148o",
-    "dreame.vacuum.p2156o",
-    "dreame.vacuum.p2041o",
-    "dreame.vacuum.p2041",
+    #"dreame.vacuum.p2140q",
+    #"dreame.vacuum.p2140p",
+    #"dreame.vacuum.p2140o",
+    #"dreame.vacuum.p2140a",
+    #"dreame.vacuum.p2140",
+    #"dreame.vacuum.p2148o",
+    #"dreame.vacuum.p2156o",
+    #"dreame.vacuum.p2041o",
+    #"dreame.vacuum.p2041",
     "dreame.vacuum.p2008",
 ]
 
